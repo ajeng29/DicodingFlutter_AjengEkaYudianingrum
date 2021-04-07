152bce47b671d3f6a432c76799646cb5138511ab
= <=>
2021-04-07 14:55:27 +0700

    movie_list

---
+++ lib/pages/movie_list.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class MovieList extends StatefulWidget{
+  @override
+  _MovieListState createState()=> _MovieListState();
+}
+
+class _MovieListState extends State<MovieList>{
+  @override
+  Widget build(BuildContext contxt){
+  return Container();
+  }
+}
