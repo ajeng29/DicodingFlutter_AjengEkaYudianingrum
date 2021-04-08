c0625467ea655fcc654159e370e287744da6c388
= <=>
2021-04-08 11:06:03 +0700

    movie list

--- lib/pages/movie_list.dart
+++ lib/pages/movie_list.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:praktikum7/service/http_service.dart';
 
 class MovieList extends StatefulWidget{
   @override
@@ -8,8 +9,27 @@ class MovieList extends StatefulWidget{
 }
 
 class _MovieListState extends State<MovieList>{
+  String result ="";
+  HttpService service;
+
   @override
-  Widget build(BuildContext contxt){
-  return Container();
+  Widget build(BuildContext context){
+    service.getPopularMovies().then((value) => {
+            setState((){
+              result = value;
+            })
+    });
+    return Scaffold(
+      appBar:  AppBar(
+        title: Text("Popular Movies"),
+      ),
+      body: Container(
+        child: Text(result),
+      )
+    );
   }
+      void initState(){
+        service = HttpService();
+        super.initState();
+      }
 }
